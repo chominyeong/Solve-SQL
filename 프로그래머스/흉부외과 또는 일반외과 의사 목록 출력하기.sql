a83c17557ddfe8998a637b61f872785d76d21b00
minrong <81460659+chominyeong@users.noreply.github.com>
2024-10-25 11:00:08 +0900

    241024

--- 프로그래머스/흉부외과 또는 일반외과 의사 목록 출력하기.sql
+++ 프로그래머스/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -1,4 +1,5 @@
-/* sql 날짜 포맷 */
+/* https://school.programmers.co.kr/learn/courses/30/lessons/132203 */
+/* sql 날짜 포맷 공부 필요 */
 
 SELECT DR_NAME, DR_ID, MCDP_CD, date_format(HIRE_YMD, '%Y-%m-%d')
 FROM DOCTOR
