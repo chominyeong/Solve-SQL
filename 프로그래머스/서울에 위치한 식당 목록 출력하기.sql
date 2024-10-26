b5d82bcbb59d553f2f07f6560d7996b0119f51c4
minrong <81460659+chominyeong@users.noreply.github.com>
2024-10-27 08:39:08 +0900

    241027

---
+++ 프로그래머스/서울에 위치한 식당 목록 출력하기.sql
@@ -0,0 +1,15 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/131118 */
+
+SELECT A.REST_ID, A.REST_NAME, A.FOOD_TYPE, A.FAVORITES, A.ADDRESS, ROUND(AVG(B.REVIEW_SCORE),2) as SCORE
+FROM REST_INFO as A
+INNER JOIN REST_REVIEW as B
+ON A.REST_ID = B.REST_ID
+WHERE A.ADDRESS LIKE '서울%'
+GROUP BY A.REST_ID
+ORDER BY SCORE DESC, A.FAVORITES DESC
+
+
+
+/* 1. 별칭 - as 뒤에 따옴표 안붙여도 됨 */
+/* 2. JOIN - 어떤 테이블에 어떤 테이블을 JOIN해야 하는지, 정보가 중복되어야 하는지 or 중복되면 안되는지 고민하고 JOIN */
+/* 3. GROUP BY - 조인 후 어떤 것(A.REST_ID)을 기준으로 산술계산을 해야 하는지 확인 */
