d63068bb5ea21fbe97a83fb655b0812eeccab808
minrong <81460659+chominyeong@users.noreply.github.com>
2024-11-02 11:32:05 +0900

    241102

---
+++ 프로그래머스/동명 동물 수 찾기.sql
@@ -0,0 +1,8 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/59041 */
+
+SELECT NAME, COUNT(NAME) "COUNT"
+FROM ANIMAL_INS
+WHERE NAME IS NOT NULL 
+GROUP BY NAME
+HAVING COUNT(NAME) >= 2
+ORDER BY 1
