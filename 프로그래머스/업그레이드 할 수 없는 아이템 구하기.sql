3b5387f987f6b72c79f0e954d6ee444ea58833d7
minrong <81460659+chominyeong@users.noreply.github.com>
2024-11-06 08:25:44 +0900

    241106

--- 프로그래머스/업그레이드 할 수 없는 아이템 구하기.sql
+++ 프로그래머스/업그레이드 할 수 없는 아이템 구하기.sql
@@ -5,3 +5,7 @@ SELECT ITEM_ID, ITEM_NAME, RARITY
 FROM ITEM_INFO
 WHERE ITEM_ID NOT IN (SELECT PARENT_ITEM_ID FROM ITEM_TREE WHERE PARENT_ITEM_ID IS NOT NULL)
 ORDER BY 1 DESC
+
+
+-- IN의 경우 or 연산으로 하나라도 =의 결과가 true이면 해당 데이터가 조회되는 것인데, NOT IN의 경우 and 연산이 된다고 한다.
+-- 즉 서브쿼리에 있는 모든 데이터에 대해서 !=의 결과가 true인 데이터가 조회된다.
