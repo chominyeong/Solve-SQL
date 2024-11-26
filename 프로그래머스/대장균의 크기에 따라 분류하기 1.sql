7d181c4a854af96a3bf1a830036b8b5a61374691
minrong <81460659+chominyeong@users.noreply.github.com>
2024-11-26 18:33:59 +0900

    241126

---
+++ 프로그래머스/대장균의 크기에 따라 분류하기 1.sql
@@ -0,0 +1,11 @@
+/* https://school.programmers.co.kr/learn/courses/30/lessons/299307 */
+
+SELECT 
+    ID, 
+    CASE 
+        WHEN SIZE_OF_COLONY <= 100 THEN "LOW"
+        WHEN (100 < SIZE_OF_COLONY) AND (SIZE_OF_COLONY <= 1000) THEN "MEDIUM"
+        ELSE "HIGH"
+    END AS SIZE
+FROM ECOLI_DATA
+ORDER BY 1
