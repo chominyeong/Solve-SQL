8a8842e32ffc5f1699c3f06cc7b7ec11358fd238
minrong <81460659+chominyeong@users.noreply.github.com>
2024-11-29 19:28:25 +0900

    241129

--- 프로그래머스/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/대장균의 크기에 따라 분류하기 2.sql
@@ -15,5 +15,6 @@ FROM RANK_DATA
 ORDER BY 1
 
 
--- 1. WITH절
+-- 1. WITH절 `WITH [생성할 테이블명] AS (서브쿼리)`
+    -- 만약 여러 개의 테이블을 생성하고 싶을 경우 `WITH [테이블명1] AS (서브쿼리), [테이블명2] AS (서브쿼리)`
 -- 2. PERCENT_RANK() OVER ()
